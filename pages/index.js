6d028b27d32091391da9dda7e824ffa8c565b806
Sayan Mukherjee <hello@sayanmukherjee.com>
2021-06-06 19:28:11 +0530

    added tailwind

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,25 @@
-import Image from "next/image";
 import Link from "next/link";
 import React from "react";
-import Footer from "../components/Footer";
-import Navbar from "../components/Navbar";
-import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <React.Fragment>
       <div>
-        <h1 className={styles.title}>hello soumi</h1>
-        <p className={styles.text}>
+        <h1>hello soumi</h1>
+        <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Et inventore
           minus eum? Expedita necessitatibus animi ipsam numquam quis officiis
           amet molestiae natus, quidem temporibus tenetur dolor! Corporis
           laudantium saepe optio.
         </p>
-        <p className={styles.text}>
+        <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Et inventore
           minus eum? Expedita necessitatibus animi ipsam numquam quis officiis
           amet molestiae natus, quidem temporibus tenetur dolor! Corporis
           laudantium saepe optio.
         </p>
         <Link href="/posts">
-          <a className={styles.btn}>See Lisitng</a>
+          <a>See Lisitng</a>
         </Link>
       </div>
     </React.Fragment>
