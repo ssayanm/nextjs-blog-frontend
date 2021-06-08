49afaa10d34c79f70e1bf73becf0e9460f741915
Sayan Mukherjee <hello@sayanmukherjee.com>
2021-06-08 11:36:39 +0530

    still working on nav

--- components/Header.js
+++ components/Header.js
@@ -11,78 +11,73 @@ const Header = () => {
 
   return (
     <>
-      <div className="bg-green-400">
-        <nav className="container mx-auto flex items-center flex-wrap  p-3 ">
-          <Link href="/">
-            <a className="inline-flex items-center p-2 mr-4 ">
-              <svg
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-                className="fill-current text-white h-8 w-8 mr-2"
-              >
-                <path d="M12.001 4.8c-3.2 0-5.2 1.6-6 4.8 1.2-1.6 2.6-2.2 4.2-1.8.913.228 1.565.89 2.288 1.624C13.666 10.618 15.027 12 18.001 12c3.2 0 5.2-1.6 6-4.8-1.2 1.6-2.6 2.2-4.2 1.8-.913-.228-1.565-.89-2.288-1.624C16.337 6.182 14.976 4.8 12.001 4.8zm-6 7.2c-3.2 0-5.2 1.6-6 4.8 1.2-1.6 2.6-2.2 4.2-1.8.913.228 1.565.89 2.288 1.624 1.177 1.194 2.538 2.576 5.512 2.576 3.2 0 5.2-1.6 6-4.8-1.2 1.6-2.6 2.2-4.2 1.8-.913-.228-1.565-.89-2.288-1.624C10.337 13.382 8.976 12 6.001 12z" />
-              </svg>
-              <span className="text-xl text-white font-bold uppercase tracking-wide">
-                Talwind CSS
-              </span>
-            </a>
-          </Link>
-          <button
-            className=" inline-flex p-3 hover:bg-green-600 rounded lg:hidden text-white ml-auto hover:text-white outline-none"
-            onClick={handleClick}
-          >
+      <header class="text-gray-600 body-font">
+        <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+          <div class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0 justify-end w-full">
             <svg
-              className="w-6 h-6"
+              xmlns="http://www.w3.org/2000/svg"
               fill="none"
               stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              class="w-10 h-10 text-white p-2 bg-green-500 rounded-full"
               viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
             >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M4 6h16M4 12h16M4 18h16"
-              />
+              <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
             </svg>
-          </button>
-          {/*Note that in this div we will use a ternary operator to decide whether or not to display the content of the div  */}
-          <div className="inline-flex">
-            <h2 className="text-center text-5xl">
-              Hello Sayan tou are the best
-            </h2>
+            <span class="ml-3 text-xl">Tailblocks</span>{" "}
+            <button
+              className=" inline-flex p-3 hover:bg-green-600 rounded lg:hidden text-white ml-auto hover:text-white outline-none"
+              onClick={handleClick}
+            >
+              <svg
+                className="w-6 h-6"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M4 6h16M4 12h16M4 18h16"
+                />
+              </svg>
+            </button>
           </div>
 
           <div
             className={`${
               active ? "" : "hidden"
-            }   w-full lg:inline-flex lg:flex-grow lg:w-auto`}
+            }   lg:flex w-max lg:flex-grow lg:justify-around`}
           >
-            <div className="lg:inline-flex lg:flex-row lg:ml-auto lg:w-auto w-full lg:items-center items-start  flex flex-col lg:h-auto">
-              <Link href="/">
-                <a className="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center hover:bg-green-600 hover:text-white ">
-                  Home
-                </a>
-              </Link>
-              <Link href="/">
-                <a className="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center hover:bg-green-600 hover:text-white">
-                  Services
-                </a>
-              </Link>
-              <Link href="/">
-                <a className="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center hover:bg-green-600 hover:text-white">
-                  About us
-                </a>
-              </Link>
-              <Link href="/">
-                <a className="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center hover:bg-green-600 hover:text-white">
-                  Contact us
-                </a>
-              </Link>
-            </div>
+            {" "}
+            <nav class="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
+              <a class="mr-5 hover:text-gray-900">First Link</a>
+              <a class="mr-5 hover:text-gray-900">Second Link</a>
+              <a class="mr-5 hover:text-gray-900">Third Link</a>
+              <a class="mr-5 hover:text-gray-900">Fourth Link</a>
+            </nav>
           </div>
-        </nav>
-      </div>
+
+          <button class="hidden lg:inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">
+            Button
+            <svg
+              fill="none"
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              class="w-4 h-4 ml-1"
+              viewBox="0 0 24 24"
+            >
+              <path d="M5 12h14M12 5l7 7-7 7"></path>
+            </svg>
+          </button>
+        </div>
+      </header>
     </>
   );
 };
