02e5f0d4f796431265e8c3c432d485f081ea789e
Sayan Mukherjee <hello@sayanmukherjee.com>
2021-06-08 16:43:17 +0530

    updating work done till now

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import Image from "next/image";
+
 import { useState } from "react";
 
 const Header = () => {
@@ -10,75 +10,88 @@ const Header = () => {
   };
 
   return (
-    <>
-      <header class="text-gray-600 body-font">
-        <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-          <div class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0 justify-end w-full">
+    <section className="bg-green-400">
+      <nav className="container flex items-center flex-wrap  p-3 mx-auto">
+        <Link href="/">
+          <a className="inline-flex items-center p-2 mr-4 ">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
-              stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              class="w-10 h-10 text-white p-2 bg-green-500 rounded-full"
               viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              className="w-10 h-10 text-gray-700 p-2 bg-gray-100 rounded-full hover:bg-green-100"
             >
-              <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M14.828 14.828a4 4 0 01-5.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+              />
             </svg>
-            <span class="ml-3 text-xl">Tailblocks</span>{" "}
-            <button
-              className=" inline-flex p-3 hover:bg-green-600 rounded lg:hidden text-white ml-auto hover:text-white outline-none"
-              onClick={handleClick}
-            >
-              <svg
-                className="w-6 h-6"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M4 6h16M4 12h16M4 18h16"
-                />
-              </svg>
-            </button>
-          </div>
 
-          <div
-            className={`${
-              active ? "" : "hidden"
-            }   lg:flex w-max lg:flex-grow lg:justify-around`}
+            <span className="ml-3 text-xl text-white font-bold uppercase tracking-wide hover:text-green-100">
+              Sayan Mukherjee
+            </span>
+          </a>
+        </Link>
+        <button
+          className=" inline-flex p-3 hover:bg-green-600 rounded lg:hidden text-white ml-auto hover:text-white outline-none"
+          onClick={handleClick}
+        >
+          <svg
+            className="w-6 h-6"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
           >
-            {" "}
-            <nav class="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
-              <a class="mr-5 hover:text-gray-900">First Link</a>
-              <a class="mr-5 hover:text-gray-900">Second Link</a>
-              <a class="mr-5 hover:text-gray-900">Third Link</a>
-              <a class="mr-5 hover:text-gray-900">Fourth Link</a>
-            </nav>
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M4 6h16M4 12h16M4 18h16"
+            />
+          </svg>
+        </button>
+        {/*Note that in this div we will use a ternary operator to decide whether or not to display the content of the div  */}
+        <div
+          className={`${
+            active ? "" : "hidden"
+          }   w-full lg:inline-flex lg:flex-grow lg:w-auto`}
+        >
+          <div className="lg:inline-flex lg:flex-row lg:ml-auto lg:w-auto w-full lg:items-center items-start  flex flex-col lg:h-auto">
+            <Link href="/">
+              <a className="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center hover:bg-green-600 hover:text-white ">
+                Home
+              </a>
+            </Link>
+            <Link href="/services">
+              <a className="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center hover:bg-green-600 hover:text-white">
+                Services
+              </a>
+            </Link>
+            <Link href="/about">
+              <a className="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center hover:bg-green-600 hover:text-white">
+                About us
+              </a>
+            </Link>
+            <Link href="/posts">
+              <a className="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center hover:bg-green-600 hover:text-white">
+                Blog
+              </a>
+            </Link>
+            <Link href="/contact">
+              <a className="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center hover:bg-green-600 hover:text-white">
+                Contact us
+              </a>
+            </Link>
           </div>
-
-          <button class="hidden lg:inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">
-            Button
-            <svg
-              fill="none"
-              stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              class="w-4 h-4 ml-1"
-              viewBox="0 0 24 24"
-            >
-              <path d="M5 12h14M12 5l7 7-7 7"></path>
-            </svg>
-          </button>
         </div>
-      </header>
-    </>
+      </nav>
+    </section>
   );
 };
 
