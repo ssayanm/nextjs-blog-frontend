74876447dc1b2d0102dc284cd4f9f5d3cf83d1e5
Sayan Mukherjee <hello@sayanmukherjee.com>
2021-06-09 08:43:02 +0530

    updated info section

--- components/Info.js
+++ components/Info.js
@@ -1,23 +1,9 @@
 import Image from "next/image";
+import { FaDumbbell, FaHdd, FaUsers } from "react-icons/fa";
 
 const Info = () => {
   return (
     <section className="relative py-20 bg-black text-white">
-      <div
-        className="bottom-auto top-0 left-0 right-0 w-full absolute pointer-events-none overflow-hidden -mt-20"
-        style={{ height: "80px" }}
-      >
-        <svg
-          className="absolute bottom-0 overflow-hidden"
-          xmlns="http://www.w3.org/2000/svg"
-          preserveAspectRatio="none"
-          version="1.1"
-          viewBox="0 0 2560 100"
-          x="0"
-          y="0"
-        ></svg>
-      </div>
-
       <div className="container mx-auto px-4">
         <div className="items-center flex flex-wrap">
           <div className="w-full md:w-4/12 ml-auto mr-auto px-4">
@@ -47,43 +33,34 @@ const Info = () => {
               <ul className="list-none mt-6">
                 <li className="py-2">
                   <div className="flex items-center">
-                    <div>
-                      <span className="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-pink-600 bg-pink-200 mr-3">
-                        <i className="fas fa-fingerprint"></i>
-                      </span>
+                    <div className="font-semibold inline-block text-orange-600  mr-3">
+                      <FaDumbbell className="text-2xl" />
                     </div>
-                    <div>
-                      <h4 className="text-xl">
-                        The latest & greatest gym equipment
-                      </h4>
+
+                    <div className="text-xl">
+                      The latest & greatest gym equipment
                     </div>
                   </div>
                 </li>
                 <li className="py-2">
                   <div className="flex items-center">
-                    <div>
-                      <span className="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-pink-600 bg-pink-200 mr-3">
-                        <i className="fab fa-html5"></i>
-                      </span>
+                    <div className="font-semibold inline-block text-orange-600  mr-3">
+                      <FaHdd className="text-2xl" />
                     </div>
-                    <div>
-                      <h4 className="text-xl">
-                        10-inch, quality foam floor padding{" "}
-                      </h4>
+
+                    <div className="text-xl">
+                      10-inch, quality foam floor padding
                     </div>
                   </div>
                 </li>
                 <li className="py-2">
                   <div className="flex items-center">
-                    <div>
-                      <span className="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-pink-600 bg-pink-200 mr-3">
-                        <i className="far fa-paper-plane"></i>
-                      </span>
+                    <div className="font-semibold inline-block text-orange-600  mr-3">
+                      <FaUsers className="text-2xl" />
                     </div>
-                    <div>
-                      <h4 className="text-xl">
-                        3 professional female and 2 male trainers
-                      </h4>
+
+                    <div className="text-xl">
+                      3 professional female and 2 male trainers
                     </div>
                   </div>
                 </li>
